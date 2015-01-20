407796bcae44d3413524e1a2d3abb15fdb0b448a
skootdiggity <smmiller.work@gmail.com>
2015-01-20 11:50:42 -0700

    complete

--- js/controller.js
+++ js/controller.js
@@ -1,6 +1,6 @@
 var app = angular.module('userProfiles');
 
-app.controller = ('MainController', function($scope, mainService) {
+app.controller('MainController', function($scope, mainService) {
 
 	$scope.getUsers = function () {
 		$scope.users = mainService.getUsers();
